8800e2b39b0dbfa1bdd43f8278272f9aa0c1b652
l27001 <l27001@ezdomain.ru>
2025-04-19 23:57:14 +0400

    slight improvement

--- src/main/java/org/example/VacationCalc/service/VacationService.java
+++ src/main/java/org/example/VacationCalc/service/VacationService.java
@@ -11,8 +11,6 @@ import org.example.VacationCalc.config.HolidayConfig;
 @Service
 public class VacationService {
 
-    private final double AVG_DAYS_IN_MONTH = 29.3;
-
     private final List<LocalDate> holidays;
 
     public VacationService(HolidayConfig holidayConfig) {
@@ -21,7 +19,8 @@ public class VacationService {
     }
 
     public double calculate(double avgSalary, int vacationDays) {
-        double avgDaySalary = avgSalary / 12 / AVG_DAYS_IN_MONTH;
+        double avgDaysInMonth = 29.3;
+        double avgDaySalary = avgSalary / 12 / avgDaysInMonth;
         return Math.round(avgDaySalary * vacationDays * 100.0) / 100.0;
     }
 
@@ -36,8 +35,7 @@ public class VacationService {
             currentDate = currentDate.plusDays(1);
         }
 
-        double avgDaySalary = avgSalary / 12 / AVG_DAYS_IN_MONTH;
-        return Math.round(avgDaySalary * payableDays * 100.0) / 100.0;
+        return calculate(avgSalary, payableDays);
     }
 
     private boolean isWeekend(LocalDate date) {
